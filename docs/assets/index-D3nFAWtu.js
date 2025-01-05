11774f7fc5a71875f51b6827d584c6ecf547ebac
Anderson Rissardi <anderson.rissardi@hotmail.com>
2025-01-05 13:25:41 -0300

    chore: configuração do vite adicionada

--- docs/assets/index-D3nFAWtu.js
+++ docs/assets/index-D3nFAWtu.js
@@ -49,7 +49,7 @@
   `}function p(){return`
     <section class="flex justify-center" style="padding-top: 3rem;">
       <img 
-        src="/images/nome-casal.png" 
+        src="./images/nome-casal.png" 
         alt="Nomes do Casal" 
         class="w-3/4 md:w-2/3 lg:w-1/2 h-auto"
       />
---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/casamento/', // Substitua pelo nome do seu repositório
+  // Outras configurações podem ser adicionadas aqui
+});
