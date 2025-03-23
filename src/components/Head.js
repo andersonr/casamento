04b3dd173e98a6aad6b41830016e2c9aae3cdd5f
Anderson Rissardi <anderson.rissardi@hotmail.com>
2025-03-23 11:11:13 -0300

    feat: added cache control

---
+++ src/components/Head.js
@@ -0,0 +1,14 @@
+export function createHead() {
+  return `
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+      <meta http-equiv="Pragma" content="no-cache">
+      <meta http-equiv="Expires" content="0">
+      <title>Anderson & Bruna - Casamento</title>
+      <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap" rel="stylesheet">
+      <link href="./styles/output.css" rel="stylesheet">
+    </head>
+  `;
+}
--- src/main.js
+++ src/main.js
@@ -1,28 +1,20 @@
 import { createGallery } from "./components/Gallery.js";
 import { createGiftList } from "./components/GiftList.js";
 import { createHeader } from "./components/Header.js";
-import { createImageSection } from "./components/ImageSection.js";
 import { createLayout } from "./components/Layout.js";
 import { createSaveTheDate } from "./components/SaveTheDate.js";
 import { createHero } from "./components/Hero.js"; // Importando o Hero
+import { createHead } from "./components/Head.js";
 import "./style.css";
 
 const path = window.location.pathname;
 
 let content = "";
 
-if (path.includes("lista-presentes.html")) {
 	content = `
-    ${createHeader(
-			"Lista de Presentes",
-			"Graças a deus, já conseguimos mobiliar a nossa casinha, mas se você quiser nos presentear, aceitamos presentes em dinheiro para a nossa lua de mel e para ajudar a pagar nossa casa :)",
-		)}
-    ${createGiftList()}
-  `;
-} else {
-	content = `
+    ${createHead()}
     ${createHero()} 
-    ${createImageSection()}
+    
     ${createSaveTheDate()}
     ${createGallery()}    
     ${createHeader(
@@ -31,8 +23,9 @@ if (path.includes("lista-presentes.html")) {
 		)}
     ${createGiftList()}
   `;
-}
 
+
+//${createImageSection()} // Imagem com nome do casal
 //${createVideoSection()}
 //${createLinkToGiftList()}
 
